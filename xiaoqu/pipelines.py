258b2498d289f1fa1c6156e0577afa5e0a42c138
fangjiyuan <36502693+fangjiyuan@users.noreply.github.com>
2022-01-10 09:50:24 +0800

    Update pipelines.py

--- xiaoqu/pipelines.py
+++ xiaoqu/pipelines.py
@@ -18,12 +18,12 @@ class ExcelPipeline(object):
     def __init__(self):
         self.wb = Workbook()
         self.ws = self.wb.active
-        self.ws.append(['省', '市', '区', '小区名', '小区详情页链接', '详细地址', '经纬度', '交通',
+        self.ws.append(['地址编码','省', '市', '区', '小区名', '小区详情页链接', '详细地址', '经纬度', '交通',
                         '参考价格', '物业类型', '物业费', '总建面积', '总户数', '竣工时间', '停车位', '容积率', '绿化率',
                         '物业公司', '开发商'])
 
     def process_item(self, item, spider):
-        line = [item['province'], item['city'], item['district'], item['name'], item['url'], item['detail_address'],
+        line = [item['adcode'], item['province'], item['city'], item['district'], item['name'], item['url'], item['detail_address'],
                 item['coord'], item['traffic'], item['price'], item['property_type'],
                 item['property_fee'], item['area'], item['house_count'], item['completion_time'], item['parking_count'],
                 item['plot_ratio'], item['greening_rate'], item['property_company'], item['developers']]
